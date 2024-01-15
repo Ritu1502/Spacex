8fe0901234b21eeb6dcb0a85e8207fb0e2fa53b7
Ritu Vyaghrambare <101789137+Ritu1502@users.noreply.github.com>
2024-01-15 16:25:20 +0530

    Update jupyter-labs-spacex-data-collection-api.py

--- jupyter-labs-spacex-data-collection-api.py
+++ jupyter-labs-spacex-data-collection-api.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# <center>
-#     <img src="https://gitlab.com/ibm/skills-network/courses/placeholder101/-/raw/master/labs/module%201/images/IDSNlogo.png" width="300" alt="cognitiveclass.ai logo"  />
-# </center>
-# 
-
 # # **SpaceX  Falcon 9 first stage Landing Prediction**
 # 
 
 # # Lab 1: Collecting the data
-# 
 
-# Estimated time needed: **45** minutes
-# 
-
-# In this capstone, we will predict if the Falcon 9 first stage will land successfully. SpaceX advertises Falcon 9 rocket launches on its website with a cost of 62 million dollars; other providers cost upward of 165 million dollars each, much of the savings is because SpaceX can reuse the first stage. Therefore if we can determine if the first stage will land, we can determine the cost of a launch. This information can be used if an alternate company wants to bid against SpaceX for a rocket launch. In this lab, you will collect and make sure the data is in the correct format from an API. The following is an example of a successful and launch.
+# In this project, I will predict if the Falcon 9 first stage will land successfully. SpaceX advertises Falcon 9 rocket launches on its website with a cost of 62 million dollars; other providers cost upward of 165 million dollars each, much of the savings is because SpaceX can reuse the first stage. Therefore if we can determine if the first stage will land, we can determine the cost of a launch. This information can be used if an alternate company wants to bid against SpaceX for a rocket launch. Here, I will collect and make sure the data is in the correct format from an API. The following is an example of a successful and launch.
 # 
 
 # ![](https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0701EN-SkillsNetwork/lab_v2/images/landing\_1.gif)
